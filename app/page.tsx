83cd57069288b96704d758f2e98c37e35b53eb92
ddmilado <adebayodamilola@gmail.com>
2024-12-10 16:55:37 +0400

    new

--- app/page.tsx
+++ app/page.tsx
@@ -49,7 +49,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-5 row-start-2 items-center sm:items-start">
-        <h2>SHILOH 2024 REGISTRATION</h2>
+        <h2>SHILOH 2024 DAY ONE REGISTRATION</h2>
         <form onSubmit={handleSubmit} className="flex flex-col items-center gap-4">
           <div className="flex flex-col w-full">
             <label className="mb-1 text-sm font-medium">Full Name</label>
