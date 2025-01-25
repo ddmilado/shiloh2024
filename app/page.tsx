f33d6d3fea17495ce3c46e3e2a7bd23183d0d5ed
ddmilado <adebayodamilola@gmail.com>
2025-01-25 20:35:05 +0400

    Added new feature

--- app/page.tsx
+++ app/page.tsx
@@ -63,7 +63,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-5 row-start-2 items-center sm:items-start">
         <h1 className="text-2xl font-bold">WCI Dubai Transport Unit</h1>
-        <h2>Bus Registration for Sunday Service.</h2>
+        <h2>Bus Registration for Sunday Service {formattedDate}</h2>
         <form onSubmit={handleSubmit} className="flex flex-col items-center gap-4">
           <div className="flex flex-col w-full">
             <label className="mb-1 text-sm font-medium">Full Name</label>
