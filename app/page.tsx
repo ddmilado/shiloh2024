632db8304684f61a7da7cfbe4f0ed95ff13bd879
ddmilado <75342606+ddmilado@users.noreply.github.com>
2025-02-22 20:15:22 +0400

    Added new feature

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
   const [isSubmitting, setIsSubmitting] = useState(false);
 
   const currentDate = new Date();
-  currentDate.setDate(currentDate.getDate());
+  currentDate.setDate(currentDate.getDate() + 1);
   const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
   const formattedDate = currentDate.toLocaleDateString(undefined, options as Intl.DateTimeFormatOptions);
 
@@ -64,7 +64,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-5 row-start-2 items-center sm:items-start">
         <h1 className="text-2xl font-bold">WCI Dubai Transport Unit</h1>
-        <h2>Bus Registration for Mid-Week Communion Service Tonight {formattedDate}</h2>
+        <h2>Bus Registration for Sunday Service Tomorrow {formattedDate}</h2>
         <form onSubmit={handleSubmit} className="flex flex-col items-center gap-4">
           <div className="flex flex-col w-full">
             <label className="mb-1 text-sm font-medium">Full Name</label>
